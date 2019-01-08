13ffa6f789b58e1e0bfee98a45389a51f6c42b66
Breno França <franciscobreno.si@gmail.com>
2019-01-07 23:41:07 -0300

    user_passsword_reset - adicionado o recurso de redefinir uma nova senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
+
 const User = use('App/Models/User')
 const Mail = use('Mail')
 
@@ -31,6 +33,41 @@ class ForgotPasswordController {
             .subject('Recuperação de senha')
         }
       )
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'O e-mail informado não foi encontrado.' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'O token de recuperação está expirado.' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+
+      await Mail.send('emails.password_reseted', {}, message => {
+        message
+          .to(user.email)
+          .from('fbsystems@gmail.com', 'Breno França | FBSystems')
+          .subject('Senha atualizada com sucesso.')
+      })
     } catch (err) {
       return response
         .status(err.status)
